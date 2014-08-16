53030ff073679ea6765fe711596aaba8ff1ed101
Michael Forrest <michael.forrest@gmail.com>
2014-08-16 23:41:31 +0200

    Notes on what pressing should do

--- GigRadio/ViewControllers/NowPlayingViewController.m
+++ GigRadio/ViewControllers/NowPlayingViewController.m
@@ -23,6 +23,8 @@
 @property (nonatomic, strong) UIPageViewController * daySelector;
 @property (nonatomic, strong) ArtistSelectionPresenter * artistsPresenter;
 
+@property (nonatomic) NSInteger currentArtistIndex;
+
 @property (nonatomic, strong) SongKickEvent * currentEvent;
 @property (nonatomic, strong) SongKickArtist * currentSongKickArtist;
 @property (nonatomic, strong) SoundCloudUser * currentSoundCloudUser;
@@ -35,6 +37,7 @@
 {
     [super viewDidLoad];
     self.date = [NSDate date];
+    self.currentArtistIndex = 0;
 }
 -(void)setDate:(NSDate *)date{
     _date = date;
@@ -57,7 +60,7 @@
     }
 }
 - (IBAction)didPressRefresh:(id)sender {
-    SongKickArtist * artist = [self.artistsPresenter.artists firstObject];
+    SongKickArtist * artist = self.artistsPresenter.artists[self.currentArtistIndex];
     if(!artist) {
         NSLog(@"NO artists!");
         return;
@@ -68,10 +71,13 @@
         RLMArray * tracks = [self.artistsPresenter artistTracks:soundCloudUser];
         
         SoundCloudTrack * track = tracks.firstObject;
-        [artistNameButton setTitle:artist.displayName forState:UIControlStateNormal];
-        [venueButton setTitle:[NSString stringWithFormat:@"%@ (%1.0fm away)", event.venue.displayName, event.distanceCache] forState:UIControlStateNormal];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [artistNameButton setTitle:artist.displayName forState:UIControlStateNormal];
+            [venueButton setTitle:[NSString stringWithFormat:@"%@ (%1.0fm away)", event.venue.displayName, event.distanceCache] forState:UIControlStateNormal];
+            
+            [nowPlayingButton setTitle:[NSString stringWithFormat:@"Now playing: %@", track.title] forState:UIControlStateNormal];
 
-        [nowPlayingButton setTitle:[NSString stringWithFormat:@"Now playing: %@", track.title] forState:UIControlStateNormal];
+        });
         
         
         self.currentEvent = event;
@@ -110,13 +116,34 @@
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:uri]];
 }
 - (IBAction)didPressArtistButton:(id)sender {
+    // do a prompt
+    // 1. open artist website
+    // 2. songkick
+    // 3. soundcloud
+    // 4. search on Spotify
+    // 5. search on iTunes
     [self open:self.currentSoundCloudUser.permalink_url];
 }
 - (IBAction)didPressVenueButton:(id)sender {
+    // do a popup, prompt:
+    // 1. songkick
+    // 2. apple maps
+    // 3. google maps
+    // 4. citymapper citymapper://directions?endcoord=51.563612,-0.073299&endname=Abney%20Park%20Cemetery&endaddress=Stoke%20Newington%20High%20Street
     [self open:[NSString stringWithFormat:@"https://www.songkick.com/venues/%li", self.currentEvent.venue.id]];
 }
 - (IBAction)didPressNowPlayingButton:(id)sender {
+    // 1. open in browser
+    // 2. open in soundcloud app
+    // 3. buy track
     [self open:self.currentSoundCloudTrack.permalink_url];
+}
+- (IBAction)didPressNext:(id)sender {
+    self.currentArtistIndex ++;
+    if(self.currentArtistIndex > self.artistsPresenter.artists.count - 1){
+        self.currentArtistIndex = 0;
+    }
+    [self didPressRefresh:nil];
 }
 
 @end

4cccc0900b49f7d76bbb80353c6242218bd6b8bc
Michael Forrest <michael.forrest@gmail.com>
2014-08-16 12:10:08 +0200

    Initial Commit

---
+++ GigRadioTests/GigRadioTests.m
@@ -0,0 +1,34 @@
+//
+//  GigRadioTests.m
+//  GigRadioTests
+//
+//  Created by Michael Forrest on 16/08/2014.
+//  Copyright (c) 2014 Good To Hear. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GigRadioTests : XCTestCase
+
+@end
+
+@implementation GigRadioTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
